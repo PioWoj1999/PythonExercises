c6991d8673fc2a3c3b63be6f5ebd1304be4a4e2c
PioWoj1999 <piotr.wojda.1999@gmail.com>
2023-08-26 12:15:22 +0200

    formatted to docstring

--- codingbat/List_2/big_diff.py
+++ codingbat/List_2/big_diff.py
@@ -1,7 +1,13 @@
-# Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
+"""Given an array length 1 or more of ints, return the difference 
+between the largest and smallest values in the array. 
+Note: the built-in min(v1, v2) and max(v1, v2) functions return the 
+smaller or larger of two values."""
 
 
 def main():
+    """
+    Main function.
+    """
     return None
 
 
